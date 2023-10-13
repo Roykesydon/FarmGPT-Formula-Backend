9ec637803ab08cea6b6637378beaf85bf1062adb
Roykesydon <roy23381547@gmail.com>
2023-10-13 23:12:32 +0800

    feat: CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, Flask, jsonify, request
+from flask_cors import CORS
 
 from blueprints.formula import formula_blueprints
 from blueprints.input_variable import input_variable_blueprints
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(formula_blueprints, url_prefix="/formula")
 app.register_blueprint(input_variable_blueprints, url_prefix="/input_variable")
